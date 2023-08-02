c95f4ce3bf37280b5c4eac8b436c4c838ca444c4
NguyenThePhong <nguyenthephong_t65@hus.edu.vn>
2023-08-02 16:02:16 +0700

    error handler

---
+++ middlewares/globalErrorHandler.js
@@ -0,0 +1,21 @@
+const global = (err, req, res, next) => {
+    const stack = err.stack;
+    const message = err.message;
+    const status = err.status ? err.status : "failed";
+    const statusCode = err.statusCode ? err.statusCode : 500;
+    res.status(statusCode).json({
+        status,
+        message,
+        stack,
+    });
+};
+
+const notFound = (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl} on the server`);
+    next(err);
+};
+
+module.exports = {
+    global,
+    notFound,
+};
