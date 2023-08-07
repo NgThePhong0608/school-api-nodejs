dde612bdf25d2094355231337bc05e33ac639978
NguyenThePhong <nguyenthephong_t65@hus.edu.vn>
2023-08-07 10:01:36 +0700

    create student middlewares

---
+++ middlewares/isStudent.js
@@ -0,0 +1,15 @@
+const Student = require("../model/Staff/Student");
+
+const isAdmin = async (req, res, next) => {
+    // find user
+    const stu_id = req?.userAuth?._id;
+    // console.log(stu_id);
+    const studentFound = await Student.findById(stu_id);
+    if (studentFound?.role === "student") {
+        next();
+    } else {
+        next(new Error("Access denied, student only !"));
+    }
+};
+
+module.exports = isAdmin;
---
+++ middlewares/isStudentLogin.js
@@ -0,0 +1,29 @@
+const Student = require("../model/Staff/Student");
+const verifyToken = require("../utils/verifyToken");
+
+const isStudentLogin = async (req, res, next) => {
+    // get token from header
+    const header = req.headers;
+    // console.log(header.authorization);
+    // const token = header.authorization.split(" ")[1];
+    const token =
+        header && header.authorization && header.authorization.split(" ")[1];
+    console.log(token);
+    // verify token
+    const verify = verifyToken(token);
+    // console.log(verify);
+    // save the user into req.obj
+    if (verify) {
+        const student = await Student.findById(verify.id).select(
+            "name email role"
+        );
+        // save to the req.user
+        req.userAuth = student;
+        next();
+    } else {
+        const err = new Error("Token expired or invalid");
+        next(err);
+    }
+};
+
+module.exports = isStudentLogin;
